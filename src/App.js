543e59f71000797a18b5629be8cafd127ca32f4b
xKouen <ejya_10@hotmail.com>
2023-08-08 20:28:22 -0700

    Cambios minimos de estructura y elementos sin usar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./scenes/dashboard";
 import Programs from "./scenes/programs";
 import Periods from "./scenes/periods";
 import Login from "./scenes/login";
 import Users from "./scenes/users";
 import { AuthProvider } from "./context/authProvider";
-import useAuth from "./hooks/useAuth";
 
 function App() {
-  const auth = useAuth();
-  console.log(auth);
   return (
     <>
       <AuthProvider>
